62c244b27d9f992387724c6b4d817c68be444728
daivq53cd3 <daivq53cd3@gmail.com>
2016-02-01 15:48:51 +0700

    addkws

--- vnw/vnw/spiders/mywork.py
+++ vnw/vnw/spiders/mywork.py
@@ -19,20 +19,29 @@ date_post = u'Ngày cập nhật'
 class MyworkSpider(scrapy.Spider):
     name = "mywork"
     allowed_domains = ["mywork.com.vn"]
-    start_urls = ["http://mywork.com.vn/tim-viec-lam/python.html"]
+    start_urls = [("http://mywork.com.vn/tim-viec-lam/" + kw + ".html")
+                  for kw in KWS]
 
     def parse(self, resp):
+        url = resp.url
+        keyword = url.split("/tim-viec-lam/")[1].split(".")[0]
         for href in resp.xpath('//div[@class="item "]/div/a/@href').extract():
-            yield scrapy.Request(resp.urljoin(href), self.parse_content)
+            request = scrapy.Request(resp.urljoin(href), self.parse_content)
+            request.meta["keyword"] = keyword
+            yield request
 
-        if resp.xpath('//div[@class="mywork-pages pagination"]/a/@class').\
-            extract()[-1] != u'disabled':
-            next_page = resp.xpath('//div[@class="mywork-pages pagination"]'
-                                    '/a/@href').extract()[-1]
-            yield scrapy.Request(resp.urljoin(next_page), self.parse)
+        try:
+            if resp.xpath('//div[@class="mywork-pages pagination"]/a/@class').\
+                extract()[-1] != u'disabled':
+                next_page = resp.xpath('//div[@class="mywork-pages pagination"'
+                                       ']/a/@href').extract()[-1]
+                yield scrapy.Request(resp.urljoin(next_page), self.parse)
+        except IndexError:
+            print "Page none!"
 
     def parse_content(self, resp):
         item = PyjobItem()
+        item["keyword"] = resp.meta["keyword"]
         item["url"] = resp.url
         item["name"] = xtract(resp, '//div[@class="title-job-info"]/text()')
         item["company"] = xtract(resp,
@@ -47,7 +56,9 @@ class MyworkSpider(scrapy.Spider):
                                            ' text-align: center;"]/text()')
         if ' ' in expiry_date:
             expiry_date = expiry_date.split(' ')[0]
-        item["expiry_date"] = parse_datetime(expiry_date)
+            item["expiry_date"] = parse_datetime(expiry_date)
+        else:
+            item["expiry_date"] = parse_datetime(expiry_date)
 
         for desjob in resp.xpath('//div[@class="desjob-company"]'):
             kws = xtract(desjob, 'h4/text()')
@@ -66,7 +77,7 @@ class MyworkSpider(scrapy.Spider):
                 else:
                     item["work"] = xtract(desjob, 'div/text()')
             if welfare == kws:
-                if not xtract(desjob, 'p/text()'):
+                if xtract(desjob, 'p/text()') != u' ':
                     item["welfare"] = xtract(desjob, 'p/text()')
                 elif xtract(desjob, 'p/text()'):
                     if xtract(desjob, 'p/span/text()') != u' ':
