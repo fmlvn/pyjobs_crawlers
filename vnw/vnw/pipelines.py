1e27e8a5942d7b6eebdc058ed53b2eb76f9581f6
Hung Nguyen Viet <hvn@familug.org>
2016-02-11 13:26:24 +0700

    add API pipeline

--- vnw/vnw/pipelines.py
+++ vnw/vnw/pipelines.py
@@ -33,3 +33,14 @@ class ElasticSearchPipeline(object):
 class VnwPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class APIPipeline(object):
+
+    collection_name = 'scrapy_items'
+
+    def __init__(self):
+        self.url = 'http://127.0.0.1:5000/python'
+
+    def process_item(self, item, spider):
+        requests.post(self.url, json=item._values)
