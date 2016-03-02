9bfceac7d29af0ca542cf8e46da276e8bc162d89
daivq53cd3 <daivq53cd3@gmail.com>
2016-03-02 12:38:36 +0700

    user_agent

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -1,11 +1,3 @@
-# Scrapy settings for vnw project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/topics/settings.html
-#
-
 BOT_NAME = 'vnw'
 BOT_VERSION = '1.0'
 
@@ -13,3 +5,7 @@ SPIDER_MODULES = ['vnw.spiders']
 NEWSPIDER_MODULE = 'vnw.spiders'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
+DOWNLOADER_MIDDLEWARES = {
+        'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
+        'vnw.rotate_useragent.RotateUserAgentMiddleware' :400
+    }
