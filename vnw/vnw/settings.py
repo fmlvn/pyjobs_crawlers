66e4b651ab959886132cd79a3f5f708db7556265
daivq <daivq53cd3@gmail.com>
2016-03-07 21:35:26 +0000

    update settings

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -1,11 +1,9 @@
 BOT_NAME = 'vnw'
-BOT_VERSION = '1.0'
+BOT_VERSION = '1.0.5'
 
 SPIDER_MODULES = ['vnw.spiders']
 NEWSPIDER_MODULE = 'vnw.spiders'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
-DOWNLOADER_MIDDLEWARES = {
-        'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
-        'vnw.rotate_useragent.RotateUserAgentMiddleware' :400
-    }
+VIETNAMWORK_USERNAME = ''
+VIETNAMWORK_PASSWORD = ''
