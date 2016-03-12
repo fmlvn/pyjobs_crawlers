2a645bef8fcfe5fe272c9b1cc372eabe361d98e4
Hung Nguyen Viet <hvn@familug.org>
2016-03-12 11:05:40 +0700

    pep8, comment unset var

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -3,11 +3,12 @@ BOT_VERSION = '1.0.5'
 
 SPIDER_MODULES = ['vnw.spiders']
 NEWSPIDER_MODULE = 'vnw.spiders'
-#USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
-USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
 ITEM_PIPELINES = {
-	'vnw.pipelines.APIPipeline': 1000,
+        'vnw.pipelines.APIPipeline': 1000,
 }
+
+USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,"
+              " like Gecko) Chrome/48.0.2564.116 Safari/537.36")
 
 # VIETNAMWORK_USERNAME = ''
 # VIETNAMWORK_PASSWORD = ''
