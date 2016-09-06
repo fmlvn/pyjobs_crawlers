6c2bd0a25a7d3c5f04c8ae602ae14730502b4941
Hung Nguyen Viet <hvn@familug.org>
2016-09-06 22:00:58 +0700

    fix datetime parsing as mywork changed UI

--- vnw/vnw/pymods.py
+++ vnw/vnw/pymods.py
@@ -3,6 +3,9 @@
 import datetime
 
 
+A_DAY_IN_SEC = 86400
+
+
 def xtract(response, xpath):
     li = []
     xtracts = response.xpath(xpath).extract()
@@ -13,8 +16,19 @@ def xtract(response, xpath):
 
 
 def parse_datetime(time):
+    '''
+    Return datetime string in YYYY-mm-dd format
+    '''
+    correct_time = str(datetime_from(time)).split(' ')[0]
+    return correct_time
+
+
+def datetime_from(time):
     post_date = str(time)
     post_date = post_date.strip()
-    correct_time = datetime.datetime.strptime(post_date, '%d-%m-%Y')
-    correct_time = str(correct_time).split(' ')[0]
-    return correct_time
+    return datetime.datetime.strptime(post_date, '%d-%m-%Y')
+
+
+def has_expired(time):
+    now = datetime.datetime.now()
+    return (datetime_from(time) - now).total_seconds() < A_DAY_IN_SEC
--- vnw/vnw/spiders/mywork.py
+++ vnw/vnw/spiders/mywork.py
@@ -2,7 +2,7 @@
 
 import scrapy
 from ..items import PyjobItem
-from ..pymods import xtract, parse_datetime
+from ..pymods import xtract, parse_datetime, has_expired
 from ..keywords import KWS
 
 province = u'Nơi làm việc'
@@ -40,54 +40,57 @@ class MyworkSpider(scrapy.Spider):
             print "Page none!"
 
     def parse_content(self, resp):
-        expiry_date = xtract(resp, '//div[@style="padding-top: 44px;'
-                                   ' text-align: center;"]/text()')
-        if ' ' not in expiry_date:
-            item = PyjobItem()
-            item["keyword"] = resp.meta["keyword"]
-            item["url"] = resp.url
-            item["name"] = xtract(resp, '//div[@class="title-job-info"]/text()'
-                                        '')
-            item["company"] = xtract(resp,
-                                     '//h1[@class="fullname-company"]/text()')
-            item["address"] = xtract(resp, '//p[@class="address-company mw-ti"'
-                                           ']/text()')
-            post_date = xtract(resp, '//div[@class="action_job sco'
-                                     're-job-company"]/ul/li[2]'
-                                     '/span/text()')
-            item["post_date"] = parse_datetime(post_date)
-            item["expiry_date"] = parse_datetime(expiry_date)
+        item = PyjobItem()
+        expiry_date = xtract(resp,
+                             '//div[@class="job_deadline"]/text()'
+                             ).split(': ')[1].strip(')')
 
-            for desjob in resp.xpath('//div[@class="desjob-company"]'):
-                kws = xtract(desjob, 'h4/text()')
-                if province == kws:
-                    item["province"] = xtract(desjob, 'span/a/text()')
-                if wage == kws:
-                    if xtract(desjob, 'span/text()'):
-                        item["wage"] = xtract(desjob, 'span/text()')
-                    else:
-                        item["wage"] = xtract(desjob, 'text()')
-                if experience == kws:
-                    item["experience"] = xtract(desjob,  'p/span/text()')
-                if work == kws:
-                    if xtract(desjob, 'p/text()') != u'':
-                        item["work"] = xtract(desjob, 'p/text()')
-                    else:
-                        item["work"] = xtract(desjob, 'div/text()')
-                if welfare == kws:
-                    if not xtract(desjob, 'p/span/text()'):
-                        item["welfare"] = xtract(desjob, 'p/span/text()')
-                    item["welfare"] = xtract(desjob, 'p/text()')
-                if specialize == kws:
-                    if xtract(desjob, 'p/text()') != u' ':
-                        item["specialize"] = xtract(desjob, 'p/text()')
-                    elif xtract(desjob, 'p/text()'):
-                        item["specialize"] = xtract(desjob, 'div/text()')
-                if file_request == kws:
-                    if len(xtract(desjob, 'p/text()')) > 10:
-                        item["file_request"] = xtract(desjob, 'p/text()')
-                    else:
-                        item["file_request"] = xtract(desjob, 'p/span/text()')
-                if language == kws:
-                    item["language"] = xtract(desjob, 'p/text()')
-            yield item
+        if has_expired(expiry_date):
+            yield {}
+        item["keyword"] = resp.meta["keyword"]
+        item["url"] = resp.url
+        item["name"] = xtract(resp, '//div[@class="title-job-info"]/text()'
+                                    '')
+        item["company"] = xtract(resp,
+                                 '//h1[@class="fullname-company"]/text()')
+        item["address"] = xtract(resp, '//p[@class="address-company mw-ti"'
+                                       ']/text()')
+        post_date = xtract(
+            resp, '//span[@class="job_update"]/text()'
+        ).split(': ')[1]
+        item["post_date"] = parse_datetime(post_date)
+        item["expiry_date"] = parse_datetime(expiry_date)
+
+        for desjob in resp.xpath('//div[@class="desjob-company"]'):
+            kws = xtract(desjob, 'h4/text()')
+            if province == kws:
+                item["province"] = xtract(desjob, 'span/a/text()')
+            if wage == kws:
+                if xtract(desjob, 'span/text()'):
+                    item["wage"] = xtract(desjob, 'span/text()')
+                else:
+                    item["wage"] = xtract(desjob, 'text()')
+            if experience == kws:
+                item["experience"] = xtract(desjob,  'p/span/text()')
+            if work == kws:
+                if xtract(desjob, 'p/text()') != u'':
+                    item["work"] = xtract(desjob, 'p/text()')
+                else:
+                    item["work"] = xtract(desjob, 'div/text()')
+            if welfare == kws:
+                if not xtract(desjob, 'p/span/text()'):
+                    item["welfare"] = xtract(desjob, 'p/span/text()')
+                item["welfare"] = xtract(desjob, 'p/text()')
+            if specialize == kws:
+                if xtract(desjob, 'p/text()') != u' ':
+                    item["specialize"] = xtract(desjob, 'p/text()')
+                elif xtract(desjob, 'p/text()'):
+                    item["specialize"] = xtract(desjob, 'div/text()')
+            if file_request == kws:
+                if len(xtract(desjob, 'p/text()')) > 10:
+                    item["file_request"] = xtract(desjob, 'p/text()')
+                else:
+                    item["file_request"] = xtract(desjob, 'p/span/text()')
+            if language == kws:
+                item["language"] = xtract(desjob, 'p/text()')
+        yield item
