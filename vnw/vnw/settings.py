0a306933d9c6e4d511b525cfbbc1fcd40b895f0f
Hung Nguyen Viet <hvn@familug.org>
2017-04-17 09:00:32 +0700

    Set ValidatePipeline as default

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -4,7 +4,7 @@ BOT_VERSION = '1.0.5'
 SPIDER_MODULES = ['vnw.spiders']
 NEWSPIDER_MODULE = 'vnw.spiders'
 ITEM_PIPELINES = {
-        'vnw.pipelines.APIPipeline': 1000,
+        'vnw.pipelines.ValidatePipeline': 500,
 }
 
 USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,"
