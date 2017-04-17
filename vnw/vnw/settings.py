3da0fc0c15103cb8fcc7e9409e4357483b2d7cdc
Hung Nguyen Viet <hvn@familug.org>
2017-04-17 11:07:46 +0700

    Decide prod on env

--- vnw/vnw/settings.py
+++ vnw/vnw/settings.py
@@ -1,3 +1,6 @@
+import os
+
+
 BOT_NAME = 'vnw'
 BOT_VERSION = '1.0.5'
 
@@ -10,11 +13,21 @@ ITEM_PIPELINES = {
 USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,"
               " like Gecko) Chrome/48.0.2564.116 Safari/537.36")
 
-# VIETNAMWORK_USERNAME = ''
-# VIETNAMWORK_PASSWORD = ''
-try:
-    import prodsettings as prod
-except ImportError:
-    pass
-else:
-    ITEM_PIPELINES.update(prod.ITEM_PIPELINES)
+is_prod = os.environ.get('PYJOBS_IS_PROD', False)
+if is_prod:
+    ITEM_PIPELINES.update({
+            'vnw.pipelines.APIPipeline': 1000,
+    })
+    DOWNLOAD_DELAY = 2
+    DOWNLOADER_MIDDLEWARES = {
+        'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,  # NOQA
+        'vnw.rotate_useragent.RotateUserAgentMiddleware': 400
+    }
+
+    try:
+        import prodsettings as prod
+    except ImportError:
+        pass
+    else:
+        VIETNAMWORK_USERNAME = prod.custom.get('VIETNAMWORK_USERNAME', '')
+        VIETNAMWORK_PASSWORD = prod.custom.get('VIETNAMWORK_PASSWORD', '')
